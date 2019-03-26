bfafa4787654dc1300a0c75aec766b862885d5ac
nadsat <antonio.martinez.c@protonmail.com>
2019-03-25 23:02:39 -0300

    ADD: test for incoming calls

--- test/ecall_control_test.exs
+++ test/ecall_control_test.exs
@@ -56,4 +56,50 @@ defmodule Ecall.ControlTest do
     Control.hang_up(pid)
     assert_receive(:ecall_disconnected, 20000)
   end
+
+  test "incoming call, hang up calling", %{sim_port: uart}do
+    {:ok, pid} = Control.start_link
+    cmds = for n <- Parser.Sim7xxx.setup_list, do: n <> "\r"
+    spawn fn ->
+      CallControlTest.send_async_ok(uart, cmds)
+    end
+    assert :ok = Control.open_device(pid, CallControlTest.control_port())
+    assert :ok = Circuits.UART.write(uart, "+CLCC: 2,1,4,0,0,\"131313\",129\r\n")
+    Circuits.UART.drain(uart)
+    assert_receive({:ecall_incoming,"131313"}, 20000)
+    spawn fn ->
+      CallControlTest.send_async_ok(uart, ["ATA\r"])
+    end
+    Control.accept pid
+    assert :ok = Circuits.UART.write(uart, "+CLCC: 2,1,0,0,0,\"131313\",129\r\n")
+    Circuits.UART.drain(uart)
+    assert_receive(:ecall_connected, 20000)
+    assert :ok = Circuits.UART.write(uart, "+CLCC: 2,1,6,0,0,\"131313\",129\r\n")
+    Circuits.UART.drain(uart)
+    assert_receive(:ecall_disconnected, 20000)
+  end
+
+  test "incoming call, hang up called", %{sim_port: uart}do
+    {:ok, pid} = Control.start_link
+    cmds = for n <- Parser.Sim7xxx.setup_list, do: n <> "\r"
+    spawn fn ->
+      CallControlTest.send_async_ok(uart, cmds)
+    end
+    assert :ok = Control.open_device(pid, CallControlTest.control_port())
+    assert :ok = Circuits.UART.write(uart, "+CLCC: 2,1,4,0,0,\"131313\",129\r\n")
+    Circuits.UART.drain(uart)
+    assert_receive({:ecall_incoming,"131313"}, 20000)
+    spawn fn ->
+      CallControlTest.send_async_ok(uart, ["ATA\r"])
+    end
+    Control.accept pid
+    assert :ok = Circuits.UART.write(uart, "+CLCC: 2,1,0,0,0,\"131313\",129\r\n")
+    Circuits.UART.drain(uart)
+    assert_receive(:ecall_connected, 20000)
+    spawn fn ->
+      CallControlTest.send_async_ok(uart, ["AT+CHUP\r"])
+    end
+    Control.hang_up(pid)
+    assert_receive(:ecall_disconnected, 20000)
+  end
 end
